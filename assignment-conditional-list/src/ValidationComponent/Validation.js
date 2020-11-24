a8ea12c831f51b23cddb5e79b8cd6b63d2bbd19b
Gilwell <gilwellm@gmail.com>
2020-11-24 09:23:43 +0300

    Conditional Listing Resolved

---
+++ assignment-conditional-list/src/ValidationComponent/Validation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const validation = (props) => {
+  let validationMessage = "text long enough";
+  if (props.inputLength <= 5) {
+    validationMessage = "Text too short";
+  }
+
+  return (
+    <div>
+      <p>{validationMessage}</p>
+      <p>{props.inputLength}</p>
+    </div>
+  );
+};
+
+export default validation;
