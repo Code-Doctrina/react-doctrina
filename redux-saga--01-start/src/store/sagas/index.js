1c336ab78e9fb02f1d14e87026f363196e940aa0
Gilwell <gilwellm@gmail.com>
2021-02-19 10:16:08 +0300

    Implemented Sagas on all action creators

--- redux-saga--01-start/src/store/sagas/index.js
+++ redux-saga--01-start/src/store/sagas/index.js
@@ -1,7 +1,26 @@
 import {takeEvery} from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
-import {logoutSaga} from './auth';
+import {
+  logoutSaga,
+  checkAuthTimeoutSaga,
+  authUserSaga,
+  authCheckStateSaga,
+} from './auth';
+import {initIgredientsSaga} from './burgerBuilder';
+import {purchaseBurgerSaga, fetchOrdersSaga} from './order';
 
 export function* watchAuth() {
+  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+  yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+  yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+}
+
+export function* watchBurgerBuilder() {
+  yield takeEvery(actionTypes.INIT_INGREDIENTS, initIgredientsSaga);
+}
+
+export function* watchOrder() {
+  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
