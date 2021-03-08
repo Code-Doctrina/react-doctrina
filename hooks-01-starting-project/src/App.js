fcba438f885709886ea706b37e7da15d83f19a45
Gilwell <gilwellm@gmail.com>
2021-03-08 09:39:51 +0300

    React Hooks

---
+++ hooks-01-starting-project/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import todo from './components/Todo';
+import Todo from './components/Todo';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Todo />
+      </div>
+    );
+  }
+}
+
+export default todo;
